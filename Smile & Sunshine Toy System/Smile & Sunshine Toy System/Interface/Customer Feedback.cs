20dbd1c9e07113506405319cfa9e6349f86d4ac0
Slt8ky <Slt8ky@gmail.com>
2025-06-29 22:17:41 +0800

    移除不必要的命名空間引用
    在 `Customer Feedback.cs` 檔案中，移除了 `Mysqlx.Crud` 和 `MySqlX.XDevAPI` 的引用，以及多個 `using System` 的引用，以簡化命名空間的使用。
    
    在 `MaterialRequirementForm.cs` 檔案中，移除了 `Google.Protobuf.WellKnownTypes` 和 `Mysqlx.Crud` 的引用，保留了 `MySql.Data.MySqlClient` 的引用以確保資料庫連接功能正常運作，同時也移除了其他不必要的 `using System` 的引用。

--- Smile & Sunshine Toy System/Smile & Sunshine Toy System/Interface/Customer Feedback.cs
+++ Smile & Sunshine Toy System/Smile & Sunshine Toy System/Interface/Customer Feedback.cs
@@ -1,14 +1,6 @@
 ﻿using MySql.Data.MySqlClient;
-using Mysqlx.Crud;
-using MySqlX.XDevAPI;
 using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Smile___Sunshine_Toy_System
